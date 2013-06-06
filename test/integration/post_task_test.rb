f215538fe25813b30f4f00f995f1faf87e134657
meyerhoffl <laura.meyerhoff@gmail.com>
2013-06-06 10:04:46 -0500

    removed unwanted line in test

--- test/integration/post_task_test.rb
+++ test/integration/post_task_test.rb
@@ -2,7 +2,6 @@ require 'test_helper'
 
 class PostTaskTest < ActionDispatch::IntegrationTest
   test "view a list of tasks" do
-    assert_equal 1, Task.count
     visit '/'
     click_link 'Add Task'
     fill_in 'Title', with: 'Buy presents'
